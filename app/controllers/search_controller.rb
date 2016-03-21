66fa864cf3b9cb7a1460a698e183b8849fcd6dfc
kiennguyenquoc <quockien.mmt06@gmail.com>
2016-03-21 15:31:03 +0700

    format js

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,9 +7,9 @@ class SearchController < ApplicationController
     keyword = escape_characters_in_string(params[:keyword])
     query = "((name:#{keyword}))"
     select_obj = Solr::Request::Select.new(nil, {'q' => query, 'wt' => "xml", 'rows' => 2000, 'indent' => true})
-    @result_total = @solr.send(select_obj)
-    @result_products = get_result_solr(@result_total)
-    @products = get_products(@result_products)
+    result_total = @solr.send(select_obj)
+    result_products = get_result_solr(result_total)
+    @products = get_products(result_products)
   end
 
   private
